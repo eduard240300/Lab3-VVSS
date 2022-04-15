38863a6de3ea034790d974592939f9675f380ff8
Eduard Blackstorm <eduard240300@gmail.com>
2022-04-15 08:18:14 +0300

    Modified

--- src/test/java/integration/IntegrationTesting.java
+++ src/test/java/integration/IntegrationTesting.java
@@ -3,6 +3,9 @@ package integration;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
@@ -18,9 +21,10 @@ import java.util.Iterator;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class IntegrationTesting {
+    Service service;
 
-    @Test
-    public void testAddStudent(){
+    @BeforeMethod
+    public void setUp(){
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
@@ -29,7 +33,12 @@ public class IntegrationTesting {
         TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    public void testAddStudent(){
+
         int result = service.saveStudent("10", "Eduard Lungu", 934);
 
         assertTrue(result == 1);
@@ -37,16 +46,6 @@ public class IntegrationTesting {
 
     @Test
     public void testAddAssignment(){
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-
         int result = service.saveTema("4", "Tema 1", 5, 2);
 
         assertTrue(result == 1);
@@ -54,16 +53,6 @@ public class IntegrationTesting {
 
     @Test
     public void testAddGrade(){
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-
         int result = service.saveNota("1", "1", 10.00, 7, "feedback");
 
         assertTrue(result == 1);
@@ -71,16 +60,6 @@ public class IntegrationTesting {
 
     @Test
     public void testAll(){
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-
         int resultAddStudent = service.saveStudent("10", "Eduard Lungu", 934);
         int resultAddAssignment = service.saveTema("4", "Tema 1", 5, 2);
         int resultAddGrade = service.saveNota("1", "1", 10.00, 7, "feedback");
